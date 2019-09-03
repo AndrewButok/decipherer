81dec3f4dad25bc35d74949402dd1da9e68c6903
abutok <andrewbutok@ukr.net>
2019-09-04 01:54:19 +0300

    Some magic with async threads

--- src/StringDecryptor.cpp
+++ src/StringDecryptor.cpp
@@ -1,42 +1,76 @@
 #include <string>
-#include <vector>
-#include <thread>
 #include "ICipher.hpp"
 #include "StringDecryptor.hpp"
 
-void StringDecryptor::decrypt_thread(int begin, int thread_count, std::string &encoded) {
-	for (size_t i = begin; i < encoded.size(); i += thread_count) {
-		std::lock_guard<std::mutex> lockGuard(this->_mutex);
-		encoded[i] = this->_cipher->decrypt(encoded[i], i);
+//void StringDecryptor::decrypt_thread(int begin, int thread_count, std::string &encoded) {
+//	for (size_t i = begin; i < encoded.size(); i += thread_count) {
+//		char c = encoded[i];
+//		c = this->_cipher->decrypt(c, i);
+//		{
+//			std::lock_guard<std::mutex> lockGuard(this->_mutex);
+//			this->_buffer.push_back(c);
+//		}
+//	}
+//}
+//
+//void StringDecryptor::encrypt_thread(int begin, int thread_count, std::string &raw) {
+//	for (size_t i = begin; i < raw.size(); i += thread_count) {
+//		char c = raw[i];
+//		c = this->_cipher->encrypt(c, i);
+//		std::this_thread::sleep_for(std::chrono::nanoseconds(50000));
+//		{
+//			std::lock_guard<std::mutex> lockGuard(this->_mutex);
+//			this->_buffer.push_back(c);
+//		}
+//	}
+//}
+std::string StringDecryptor::decrypt_thread(std::string encoded, size_t fpos, char (ICipher::*decrypt_fun)(char, size_t)) {
+//	std::cout << std::this_thread::get_id() << std::endl;
+	for (std::string::iterator position = encoded.begin(); position != encoded.end(); position++) {
+		if (!this->_cipher->isAlphabetChar(*position)) {
+			position--;
+			encoded.erase(position + 1);
+		}
 	}
-}
-
-void StringDecryptor::encrypt_thread(int begin, int thread_count, std::string &raw) {
-	for (size_t i = begin; i < raw.size(); i += thread_count) {
-		std::lock_guard<std::mutex> lockGuard(this->_mutex);
-		raw[i] = this->_cipher->encrypt(raw[i], i);
-	}
+	for (size_t position = 0; position < encoded.size(); position++)
+		encoded[position] = (this->_cipher.get()->*decrypt_fun)(
+				encoded[position], position + fpos);
+	return encoded;
+//	return (this->_cipher.get()->*decrypt_fun)(c, pos);
 }
 
 void StringDecryptor::decrypt(std::string &encoded, Mode mode) {
-	for (std::string::iterator pos = encoded.begin(); pos != encoded.end(); pos++) {
-		if (!this->_cipher->isAlphabetChar(*pos)) {
-			pos--;
-			encoded.erase(pos + 1);
-		}
+//	for (std::string::iterator pos = encoded.begin(); pos != encoded.end(); pos++) {
+//		if (!this->_cipher->isAlphabetChar(*pos)) {
+//			pos--;
+//			encoded.erase(pos + 1);
+//		}
+	char (ICipher::*decrypt_fun)(char, size_t) = mode == Mode::Normal ?
+			&ICipher::decrypt : &ICipher::encrypt;
+	for (size_t pos = 0; pos < 4; pos++) {
+		size_t	str_begin = (encoded.size() / 4) * pos,
+				char_number = encoded.size() / 4 + (pos == 3 ? encoded.size() % 4 : 0);
+		this->_buffer.push_back(
+				std::async(std::launch::async,
+					&StringDecryptor::decrypt_thread, this,
+					std::move(encoded.substr(str_begin, char_number)), str_begin, decrypt_fun));
 	}
-	std::vector<std::thread> thread_vector(4);
-	for (int i = 0; i < 4; i++) {
-		if (mode == Mode::Normal)
-			thread_vector[i] = std::thread(&StringDecryptor::decrypt_thread,
-				this, i, 4, std::ref(encoded));
-		else if (mode == Mode::Reversed)
-			thread_vector[i] = std::thread(&StringDecryptor::encrypt_thread,
-				this, i, 4, std::ref(encoded));
-	}
-	for (std::thread &thr: thread_vector)
-		if (thr.joinable())
-			thr.join();
+//	std::vector<std::thread> thread_vector(2);
+//	for (int i = 0; i < 2; i++) {
+//		if (mode == Mode::Normal)
+//			thread_vector[i] = std::thread(&StringDecryptor::decrypt_thread,
+//				this, i, 2, std::ref(encoded));
+//		else if (mode == Mode::Reversed)
+//			thread_vector[i] = std::thread(&StringDecryptor::encrypt_thread,
+//				this, i, 2, std::ref(encoded));
+//	}
+//	for (std::thread &thr: thread_vector)
+//		if (thr.joinable())
+//			thr.join();
+	encoded.clear();
+	std::for_each(_buffer.begin(), _buffer.end(), [&encoded](std::future<std::string> &c) {
+		encoded += c.get();
+	});
 }
 
 
--- src/includes/StringDecryptor.hpp
+++ src/includes/StringDecryptor.hpp
@@ -3,27 +3,32 @@
 
 #include <memory>
 #include <mutex>
+#include <vector>
+#include <future>
 #include "ICipher.hpp"
 
 class StringDecryptor {
+public:
+	enum class Mode: unsigned char {
+		Normal = 0,
+		Reversed = 1
+	};
 private:
-	CipherPtr					_cipher;
-	std::mutex					_mutex;
+	CipherPtr								_cipher;
+	std::vector<std::future<std::string>>	_buffer;
+	std::mutex								_mutex;
 
 	StringDecryptor();
 	StringDecryptor(const StringDecryptor &);
 	StringDecryptor &operator=(const StringDecryptor &);
 
-	void decrypt_thread(int begin, int thread_count, std::string &encoded);
-	void encrypt_thread(int begin, int thread_count, std::string &raw);
+//	void decrypt_thread(int begin, int thread_count, std::string &encoded);
+//	void encrypt_thread(int begin, int thread_count, std::string &raw);
+	std::string decrypt_thread(std::string encoded, size_t pos, char (ICipher::*decrypt_fun)(char, size_t));
 public:
 	explicit StringDecryptor(CipherPtr cipher);
 	~StringDecryptor();
 
-	enum class Mode: unsigned char {
-		Normal = 0,
-		Reversed = 1
-	};
 	void decrypt(std::string &encoded, Mode mode);
 };
 
